ec73dfda5056d6ff3170498ccdb27ee2fdc41ba1
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2017-07-15 19:33:12 +0900

    New: setup maven

---
+++ src/main/java/info/saxcy/dale_front/DaleFrontApplication.java
@@ -0,0 +1,12 @@
+package info.saxcy.dale_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaleFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaleFrontApplication.class, args);
+	}
+}
